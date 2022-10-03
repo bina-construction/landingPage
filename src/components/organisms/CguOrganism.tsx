dcb28965573efec8525d69eb5bcd11b008a8ae58
jeremynoh <jeremynohile@gmail.com>
2022-10-03 12:05:32 +0200

    style cgu

--- src/components/organisms/CguOrganism.tsx
+++ src/components/organisms/CguOrganism.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Markdown from "react-markdown";
 import cguJson from "../../data/cgu.json";
+import { BinaLogo } from "../atoms";
+import { Footer } from "../section";
+import "./cguOrganism.scss";
 
 interface Props {}
 
 const CguOrganism: React.FC<Props> = () => {
-  return <Markdown>{cguJson.cgu}</Markdown>;
+  return (
+    <>
+      <nav id="cgu-nav" className="cgu-nav-default-color">
+        <div className="bina-logo">
+          <a href="/">
+            <BinaLogo onClick={() => {}} size="md" />
+          </a>
+        </div>
+      </nav>
+      <div className="cgu-main">
+        <Markdown>{cguJson.cgu}</Markdown>
+      </div>
+      <Footer withoutLink />
+    </>
+  );
 };
 
 export default CguOrganism;
